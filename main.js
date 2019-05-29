906528f3d393869c28f6b5d6035d5c9bbca2e5a5
Kamon <Kamon.kirker@gmail.com>
2019-05-28 21:47:00 -0700

    parameter passed to add sales needs to reference input not moueclick

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ class Dashboard {
             id : 1,
             value : 0 
         }
-        this.salesElement = document.getElementById("dashbaordSales");
+        this.salesElement = document.getElementById("dashboardSales");
         this.salesWidget = new Widget(function(salesValue){
             if (this.dashboardValue.id !== salesValue.id) {  // Make sure they aren't adding a value which has already been added to support future updating.
                 console.log("sales updated!");
@@ -30,19 +30,22 @@ class Widget {
             value : 0 
         }
 
-        this.input = document.getElementById("widgetText");
+        this.input = document.getElementById("widgetText").value;
         this.button = document.getElementById("widgetButton");
-        this.button.addEventListener("click", function() {
-            this.AddSales(this.input.value);
-        });
+        this.button.addEventListener("click", (input) => this.AddSales(input))
+
     }
+        
 
     AddSales(value)
     {
         this.widgetValue.id += 1;
+        //console.log(this.widgetValue.id)
         this.widgetValue.value = value;
+        console.log(this.widgetValue.value)
         this.displayElement.innerHTML = this.widgetValue.value;
-        this.callback(this.widgetValue);
+        console.log(this.displayElement.innerHTML)
+        //this.callback(this.widgetValue);
     }
 
     // TODO AddColorValidation 
